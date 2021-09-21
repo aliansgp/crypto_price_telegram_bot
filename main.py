df3eab7a2a6f3491bbe4388ac6963d6559451b19
saly abdollahian <38432834+aliansgp@users.noreply.github.com>
2021-09-22 00:31:34 +0330

    'create-get_cmc_data-and-parameters'

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from tokens import cmc_token
 
 
+def get_cmc_data(crypto):
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    params = {'symbol':crypto,'convert':'USD'}
+    header = {'X-CMC_PRO_API_KEY': cmc_token}
 
 
 def main():
